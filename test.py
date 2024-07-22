cc22375e792ff873422d08bad193b7e4f4d88471
Vibrant-Oracle <divya7413@gmail.com>
2024-07-22 14:37:40 +0530

    test1

---
+++ test.py
@@ -0,0 +1,6 @@
+print("hello Github")
+#ctrl+shift+~    to open terminal
+#   1. git status- to check the haal chaal of the repository    {first command}
+# 1. git add .
+#2. git commit -m 'msg'
+#3 git push origin master  ----- agar main h to main, master to master 
