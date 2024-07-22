90469e4c2a027b6098c8a51bbda32f43eea14e32
Vibrant-Oracle <divya7413@gmail.com>
2024-07-22 15:10:36 +0530

    update successfull

--- test.py
+++ test.py
@@ -4,3 +4,16 @@ print("hello Github")
 # 1. git add .
 #2. git commit -m 'msg'
 #3 git push origin master  ----- agar main h to main, master to master 
+
+# SUMMARY
+# make repository 
+# git clone 
+# u have folder same name as repository 
+# after making file.... run those three commands to upload in github online 
+# its not cloud space it is 
+
+# you can clone someone others repository also
+
+#CMD 
+# for retrieving permanent deleted files in git 
+#git checkout -f
